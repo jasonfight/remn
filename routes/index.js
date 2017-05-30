11220cf45fbebe48d019cc2f0a37f6a844b40c4b
Jason Guo <jasonlearning@outlook.com>
2017-05-30 20:41:59 +0800

    aaaaa

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,12 @@ router.get('/', function(req, res, next) {
   res.sendfile('./views/index.html');
 });
 
-router.get('/login',function(req,res,next) {
-  res.sendfile('./views/login.html')
+router.get('/input',function(req,res,next) {
+  res.sendfile('./views/input.html')
+});
+
+router.get('/input0',function(req,res,next) {
+  res.sendfile('./views/input0.html')
 });
 
 router.get('/show',function(req,res,next) {
